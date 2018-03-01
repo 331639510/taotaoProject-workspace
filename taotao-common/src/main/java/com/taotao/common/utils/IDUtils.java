373286944eb2d94eb3aa44dbfaed82a9592c10aa
331639510 <331639510@qq.com>
2018-03-01 16:48:39 +0800

    4前台页面

---
+++ taotao-common/src/main/java/com/taotao/common/utils/IDUtils.java
@@ -0,0 +1,29 @@
+package com.taotao.common.utils;
+
+import java.util.Random;
+
+public class IDUtils {
+
+	/**
+	 * 图名生成
+	 */
+	public static String genImageName() {
+		long millis = System.currentTimeMillis();
+		Random random = new Random();
+		int end3 = random.nextInt(999);
+		
+		String str = millis + String.format("%03d", end3);
+		return str;
+	}
+	
+	public static long genItemId() {
+		long millis = System.currentTimeMillis();
+		Random random = new Random();
+		int end2 = random.nextInt(99);
+		
+		String str = millis + String.format("%02d", end2);
+		long id = new Long(str);
+		return id;
+	}
+	
+}
